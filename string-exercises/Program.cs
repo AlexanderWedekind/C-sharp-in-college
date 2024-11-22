289d37fcb0db4945fd5cd2b8da14ee110d380119
Alexander Wedekind <awedwork@gmail.com>
2024-11-22 23:36:08 +0000

    adds methods: Collect user input, detect an empty string input, Sanitize user input

--- string-exercises/Program.cs
+++ string-exercises/Program.cs
@@ -14,17 +14,41 @@ namespace strings
             String fullName = "";
             String abbreviatedName = "";
 
+            String askFirstName = "Choose your character's first name...\n(type their name, then press ENTER)";
+            String askLastName = "Thank you, now please choose your character's lastname...\n(typr their lastname, then press ENTER)";
+            String askNickName = "And finally, what is your character's nickname...\n(type their nickname, then press ENTER)";
+            String rejectEmptyInput = "You can't leave this blank. Please choose something and press ENTER";
+
+
+            
+
+            static Boolean TestForEmptyString(string input)
+            {
+                if(input == "")
+                {
+                    return true;
+                }
+                return false;
+            }    
+
             static string TrimLeadingSpaces(string name)
             {   
                 String trimmedName = name;
                 while(trimmedName.Substring(0, 1) == " ")
                 {
+                    if(trimmedName.Length > 1)
+                    {
                     trimmedName = trimmedName.Substring(1, trimmedName.Length - 1);
+                    }
+                    else
+                    {
+                        trimmedName = trimmedName.Substring(0, 0);
+                    }
                 }
                 return trimmedName;
             }
 
-            static string TrimTrailingSpace(string name)
+            static string TrimTrailingSpaces(string name)
             {
                 String trimmedName = name;
                 while(trimmedName.Substring(trimmedName.Length - 1, 1) == " ")
@@ -34,20 +58,50 @@ namespace strings
                 return trimmedName;
             }
 
-            Console.WriteLine("Choose your character's first name...\n(type their name, then press ENTER)");
-            firstName = TrimTrailingSpace(TrimLeadingSpaces(Console.ReadLine()));
+            static void AskPlayer(string message)
+            {
+                Console.WriteLine(message);
+            }
 
-            Console.WriteLine("Thank you, now please choose your character's lastname...\n(typr their lastname, then press ENTER)");
-            lastName = TrimTrailingSpace(TrimLeadingSpaces(Console.ReadLine()));
+            static string CollectPlayerInput(string question)
+            {               
+                AskPlayer(question);
+                return Console.ReadLine();
+            }
 
-            Console.WriteLine("And finally, what is your character's nickname...\n(type their nickname, then press ENTER)");
-            nickName = TrimTrailingSpace(TrimLeadingSpaces(Console.ReadLine()));
+            string SanitizedPlayerInput(string input)
+            {
+                String playerInput = input;
+                while(TestForEmptyString(TrimTrailingSpaces(TrimLeadingSpaces(playerInput))) == true)
+                {
+                    playerInput = CollectPlayerInput(rejectEmptyInput);
+                }
+                return playerInput;
+            }
+
+
+
+            
+
+
+            
+
+            
+            
+           
+
+            
+            firstName = SanitizedPlayerInput(CollectPlayerInput(askFirstName));
+
+            lastName = SanitizedPlayerInput(CollectPlayerInput(askLastName));
+
+            nickName = SanitizedPlayerInput(CollectPlayerInput(askNickName));
 
             nickNameInTheMiddle = "\"" + nickName + "\"";
 
             fullName = firstName + " " + nickNameInTheMiddle + " " + lastName;
 
-            Console.WriteLine(fullName);
+            Console.WriteLine("Welcome, " + fullName + "!");
 
             abbreviatedName = fullName.Substring(0, 1) + fullName.Substring(fullName.LastIndexOf(" "), fullName.Length - fullName.LastIndexOf(" "));
 
