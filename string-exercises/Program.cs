af11ac3286068bd7458fc551725788b87ba86e80
Alexander Wedekind <awedwork@gmail.com>
2024-11-26 01:52:05 +0000

    adds 'choose your class' method and method to check for correct player-class choice

--- string-exercises/Program.cs
+++ string-exercises/Program.cs
@@ -13,17 +13,27 @@ namespace strings
             String nickNameInTheMiddle = "";
             String fullName = "";
             String abbreviatedName = "";
+            String characterClass = "";
+            int strength = 0;
+            int dexterity = 0;
+            int willpower = 0;
+
 
             String askFirstName = "Choose your character's first name...\n(type their name, then press ENTER)";
             String askLastName = "Thank you, now please choose your character's lastname...\n(type their lastname, then press ENTER)";
             String askNickName = "And finally, what is your character's nickname...\n(type their nickname, then press ENTER)";
             String rejectEmptyInput = "You can't leave this blank. Please choose something and press ENTER";
+            String welcomeMessage = "Welcome, " + fullName + "!";
+            String announceStats = "Your stats are: \n Strength: " + strength + "\nDexterity: " + dexterity + "\nWillpower: " + willpower;
+            String askCharacterClass = "Choose the class you want to play as; Mage (M), Warrior (W) or Thief (T). \n(Type \"m\", \"w\" or \"t\", then press ENTER)";
+            String rejectInvalidClassChoice = "You must choose either Mage (M), Warrior (W) or Thief (T)\nPlease type \"m\", \"w\" or \"t\" to choose your class, then press ENTER";
+
 
             Random random = new Random();
 
 
             
-
+ 
             static Boolean TestForEmptyString(string input)
             {
                 if(input == "")
@@ -81,7 +91,14 @@ namespace strings
                 {
                     while(trimmedName.Substring(trimmedName.Length - 1, 1) == " ")
                     {
-                        trimmedName = trimmedName.Substring(0, trimmedName.Length - 1);
+                        if(trimmedName.Length > 1)
+                        {
+                            trimmedName = trimmedName.Substring(0, trimmedName.Length - 1);
+                        }
+                        else
+                        {
+                            trimmedName = trimmedName.Substring(0, 0);
+                        }
                     }
                 }
                 return trimmedName;
@@ -89,7 +106,7 @@ namespace strings
 
             static void AskPlayer(string message)
             {
-                Console.WriteLine(message);
+                Console.WriteLine("\n" + message);
             }
 
             static string CollectPlayerInput(string question)
@@ -107,6 +124,50 @@ namespace strings
                 }
                 return TrimTrailingSpaces(TrimLeadingSpaces(playerInput));
             }
+
+            Boolean CheckClassChoice(string choice)
+            {
+                Boolean judgedChoice = true;
+                switch(choice.ToLower())
+                {
+                    case "m":
+                        judgedChoice = true;
+                        break;
+                    case "w":
+                        judgedChoice = true;
+                        break;
+                    case "t":
+                        judgedChoice = true;
+                        break;
+                    default:
+                        judgedChoice = false;
+                        break;
+                }
+                return judgedChoice;
+            }
+
+            string CollectClassChoice()
+            {
+                String chosenClass = "";
+                chosenClass = SanitizedPlayerInput(CollectPlayerInput(askCharacterClass));
+                while(CheckClassChoice(chosenClass) == false)
+                {
+                    chosenClass = SanitizedPlayerInput(CollectPlayerInput(rejectInvalidClassChoice));
+                }
+                switch(chosenClass)
+                {
+                    case "m":
+                        chosenClass = "Mage";
+                        break;
+                    case "w":
+                        chosenClass = "Warrior";
+                        break;
+                    case "t":
+                        chosenClass = "Thief";
+                        break;
+                }
+                return chosenClass;
+            }
            
             firstName = SanitizedPlayerInput(CollectPlayerInput(askFirstName));
 
@@ -118,18 +179,27 @@ namespace strings
 
             fullName = firstName + " " + nickNameInTheMiddle + " " + lastName;
 
-            Console.WriteLine("Welcome, " + fullName + "!");
+            AskPlayer(welcomeMessage);
 
             abbreviatedName = fullName.Substring(0, 1) + fullName.Substring(fullName.LastIndexOf(" "), fullName.Length - fullName.LastIndexOf(" "));
 
             
-            int strength = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
-            int dexterity = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
-            int willpower = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            strength = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            dexterity = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            willpower = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
 
-            Console.WriteLine("Your stats are: \n Strength: " + strength + "\nDexterity: " + dexterity + "\nWillpower: " + willpower);
+            AskPlayer(abbreviatedName + "\n" + announceStats);
+
+
 
             
+
+            
+         
+
+
+
+
 
             
 
