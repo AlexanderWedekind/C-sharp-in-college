fc9b74c5898385fdbca2c5cbadb51f8168186f30
AlexanderWedekind <awedwork@gmail.com>
2024-11-26 10:10:06 +0000

    adds start of times tables program

---
+++ Skates-and-Adders/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+    {
+        static void Main()
+        {
+            int chosenForTimesTables = 0;
+
+            String viewTimestables = "Pick a number to see the times-tables for it, up to 12\nType a number, then press ENTER";
+            String thankYouTimesTables = "Great! Let see the times-tables for " + chosenForTimesTables;
+
+            void MessagePlayer(string message)
+            {
+                Console.WriteLine("\n" + message);
+            }
+
+            string CollectPlayerInput(string message)
+            {
+                MessagePlayer(message);
+                return Console.ReadLine();
+            }
+
+            void ShowTimestables(int number)
+            {
+                int currentTable = 1;
+                int lineInTable = 1;
+                for(int i = 0; i < 12; i++)
+                {
+                    
+                }
+            }
+
+            chosenForTimesTables = int.Parse(CollectPlayerInput(viewTimestables));
+
+            MessagePlayer(thankYouTimesTables);
+
+
+        }
+    }
