27676e8923e7482f7c4f5c1b4dca5e270d906dae
Alexander Wedekind <awedwork@gmail.com>
2024-12-02 20:12:07 +0000

    adds working times tables generator app

--- Skates-and-Adders/Program.cs
+++ Skates-and-Adders/Program.cs
@@ -5,23 +5,39 @@ class Program
 {
     public class Messages()
     {
-        public String newLine = "\n";
-        public String askForNumber = "Pick a number to see the times-tables for it, up to 12\nType a number, then press ENTER";
+        public string newLine = "\n";
+        public string welcome = "Hello there!\nWelcome to:\nTimes Tables Generator 9001 (c)\n\nYou can use this tool to generate times tables, withing a range of your choosing.";
+        public string askForNumber = "Pick a number to see the times-tables for it.";
         public string thanksForChoosingNumber(int chosenForTimesTables)
         {
-            return "Great! Let see the times-tables for " + chosenForTimesTables;
+            return "Great! Let see the times-tables for " + chosenForTimesTables + ".";
         }
-        public String rejectInvalidChoice = "That wasn't a valid choice. You have to type a number, using the number keys, then press ENTER";
-        public String positiveTodayPlease = "We're only doing positive numbers today. Choose again.";
+        public string rejectInvalidChoice = "That wasn't a valid choice.\nYou have to type a number, using the number keys, then press ENTER.";
+        public string positiveTodayPlease = "We're only doing positive numbers today. Choose again.";
+        public string explainRangeChoice(int chosenForTimesTables)
+        {
+            return "You can choose where to start and where to end generating the times table.\nFor example: if you want to see the answers from '" 
+            + chosenForTimesTables + " x 4 =' up to '" + chosenForTimesTables + " x 29 =' and all the ones inbetween,\n" 
+            + "then you pick 4 as your starting number and 29 as your end number.";
+        }
+        public string typeAndEnter = "(Type a number, then press ENTER)";
+        public string askStartNumber = "First, pick the starting number.";
+        public string askForEndNumber = "Now, pick your end number.";
+        public string thanks = "Thank you.";
+        public string announceResults = "Here are the results:";
+        public string goodbye = "Thank you for using Times Tables Generator 9001 (c)";
     }
+
+    
 
     static void Main()
     {
         Messages message = new Messages();
-        int chosenForTimesTables = 0;
 
-        
-        
+        int chosenForTimesTables = 0;
+        int startNumber = 0;
+        int endNumber = 0;
+        string[] table;
 
         void MessagePlayer(string message)
         {
@@ -34,7 +50,7 @@ class Program
             return Console.ReadLine();
         }
 
-        (bool convertible, Boolean aboveZero) testInput(string convertMe)
+        (bool convertible, bool aboveZero) testInput(string convertMe)
         {
             Boolean convertible = false;
             Boolean aboveZero = false;
@@ -51,23 +67,6 @@ class Program
             }
         }
 
-        // int SanitizedNumberChoice(string input)
-        // {
-        //     String toBeConverted = input;
-        //     int convertedInput = 0;
-        //     while(int.TryParse(toBeConverted, out int result) == false)
-        //     {
-        //         toBeConverted = CollectPlayerInput(message.rejectInvalidChoice);
-        //         while(int.TryParse(toBeConverted, out int num) && result < 0)
-        //         {
-        //             toBeConverted = CollectPlayerInput(message.positiveTodayPlease);
-        //         }
-        //     }
-        //     convertedInput = int.Parse(toBeConverted);
-        //     return convertedInput;
-        // }
-
-
         int SanitizedNumberChoice(string userInput)
         {
             string toBeConverted = userInput;
@@ -76,33 +75,63 @@ class Program
             {
                 if(testInput(toBeConverted).convertible == false)
                 {
-                    toBeConverted = CollectPlayerInput(message.rejectInvalidChoice);
+                    toBeConverted = CollectPlayerInput(message.newLine + message.rejectInvalidChoice);
                 }else{
-                    toBeConverted = CollectPlayerInput(message.positiveTodayPlease);
+                    toBeConverted = CollectPlayerInput(message.positiveTodayPlease + message.newLine + message.typeAndEnter);
                 }
             }
             convertedInput = int.Parse(toBeConverted);
+            MessagePlayer(message.newLine + message.thanks);
             return convertedInput;
         }
 
-        void ShowTimestables(int number, int startNumber, int endNumber)
+        string[] GenerateTimesTables(int number, int startNumber, int endNumber)
         {
-            int start = startNumber;
-
-            int numberOfLines = endNumber - startNumber;
-            String Table = "";
+            int numberOfLines = endNumber - startNumber + 1;
+            string[] Table = [];
             int lineInTable = startNumber;
-            String currentLine = "";
+            string currentLine = "";
+            int answer = 0;
             for(int i = 0; i < numberOfLines; i++)
             {
-                currentLine = 
+                answer = lineInTable * number;
+                currentLine = lineInTable + " x " + number + " = " + answer;
+                // Table = Table + "\n" + currentLine;
+                Table = Table.Append(currentLine).ToArray();
+                lineInTable ++;
+            }
+            return Table;
+        }
+
+        void PrintResults(string[] table)
+        {
+            for(int i = 0; i < table.Length; i++)
+            {
+                MessagePlayer(table[i]);
             }
         }
 
-        chosenForTimesTables = int.Parse(CollectPlayerInput(message.askForNumber));
+        
+
+
+        MessagePlayer(message.newLine + message.welcome);
+
+        chosenForTimesTables = SanitizedNumberChoice(CollectPlayerInput(message.newLine + message.askForNumber + message.newLine + message.typeAndEnter));
 
         MessagePlayer(message.thanksForChoosingNumber(chosenForTimesTables));
 
+        MessagePlayer(message.newLine + message.explainRangeChoice(chosenForTimesTables));
 
+        startNumber = SanitizedNumberChoice(CollectPlayerInput(message.askStartNumber));
+
+        endNumber = SanitizedNumberChoice(CollectPlayerInput(message.askForEndNumber));
+
+        table = GenerateTimesTables(chosenForTimesTables, startNumber, endNumber);
+
+        MessagePlayer(message.newLine + message.announceResults + message.newLine);
+
+        PrintResults(table);
+
+        MessagePlayer(message.newLine + message.goodbye);
     }
 }
