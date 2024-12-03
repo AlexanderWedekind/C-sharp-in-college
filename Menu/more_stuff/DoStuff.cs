f7ae39d087ff5e4e6c588e1362e1e37da77d6de3
Alexander Wedekind <awedwork@gmail.com>
2024-12-03 18:29:49 +0000

    moves methods and messages to user strings classes into different files as I figured out how to split code across multiple files using namespaces

---
+++ Menu/more_stuff/DoStuff.cs
@@ -0,0 +1,100 @@
+using MyMenuMessages;
+using MyRetirementChecker;
+
+namespace MyMenuMethods
+{
+    public class MenuMethods
+        {
+            MenuMessages message = new MenuMessages();
+            public void MessageUser(string message)
+            {
+                Console.WriteLine(message);
+            }
+
+            public string CollectUserInput(string message)
+            {
+                MessageUser(message);
+                return Console.ReadLine();
+            }
+
+            
+            (bool isNum, bool isValidChoice) CheckMenuChoice(string userInput, int[] menuRange)
+            {
+                bool isNum = false;
+                bool isValidChoice = false;
+                if(int.TryParse(userInput, out int choice))
+                {
+                    isNum = true;
+                    for(int i = 0; i < menuRange.Length; i++)
+                    {
+                        if(choice == menuRange[i])
+                        {
+                            isValidChoice = true;
+                        }
+                    }
+                }
+                return (isNum, isValidChoice);
+            }
+
+            public int CollectMenuChoice()
+            {
+                int[] menuRange = [1, 2, 3, 4, 5];
+                string userInput = CollectUserInput(message.welcome + message.newLine + message.menu + message.newLine + message.typeAndEnter);
+                while(CheckMenuChoice(userInput, menuRange).isNum == false || CheckMenuChoice(userInput, menuRange).isValidChoice == false)
+                {
+                    if(CheckMenuChoice(userInput, menuRange).isNum == false)
+                    {
+                        userInput = CollectUserInput(message.rejectNotNumber);
+                    }
+                    else
+                    {
+                        userInput = CollectUserInput(message.rejectNotAMenuChoice + message.newLine + message.typeAndEnter);
+                    }
+                }
+                return int.Parse(userInput);
+            }
+            
+            public void Exit()
+            {
+                MessageUser(message.goodBye);
+                Environment.Exit(0);
+            }
+
+            public void Menu()
+            {
+                bool session = true;
+                int menuChoice = 0;
+                while(session)
+                {
+                    MessageUser(message.helloThere);
+                    menuChoice = CollectMenuChoice();
+                    switch(menuChoice)
+                    {
+                        case 1:
+                            Console.WriteLine("menu choice 1 : Times Table Generator");
+                            break;
+                        case 2:
+                            Console.WriteLine("menu choice 2 : Orc v Dwarf Battle");
+                            break;
+                        case 3:
+                            Console.WriteLine("menu choice 3 : Magical Medieval Adventure");
+                            break;
+                        case 4:
+                            Console.WriteLine("menu choice 4 : Retirement Checker");
+
+                            RetirementChecker.RunRetirementChecker();
+                            break;
+                        case 5:
+                            Exit();
+                            break;
+                    }
+                }
+            }
+
+
+            public void SayHelloThere()
+            {
+                MessageUser(message.helloThere);
+            }
+        }
+}
---
+++ Menu/more_stuff/SayStuff.cs
@@ -0,0 +1,19 @@
+namespace MyMenuMessages
+{
+    public class MenuMessages
+        {
+            public string helloThere = "Hello there!";
+            public string welcome = "Choose what you want to do";
+            public string newLine = "\n";
+            public string rejectNotAMenuChoice = "Please pick one of the options on the menu";
+            public string typeAndEnter = "(choose by typing a number, then press ENTER)";
+            public string rejectNotNumber = "That wasn't a valid choice; pick an option by using the NUMBER keys";
+            public string menu = "Options:\n"
+            + "1) Times Table Generator\n"
+            + "2) Orc v Dwarf Battle\n"
+            + "3) Magical Medieval Adventure\n"
+            + "4) Retirement Checker\n"
+            + "5) Quit";
+            public string goodBye = "Thank you. See you next time.";
+        }
+}
