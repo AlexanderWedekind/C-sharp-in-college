a74da8df01ee755b53f9bf956f7d1fcb99ce675e
Alexander Wedekind <awedwork@gmail.com>
2025-02-13 13:20:23 +0000

    adds array exercises

---
+++ array-exercises-2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace arrayExercises2
+{
+    struct Message
+    {
+        public static string NewLine = "\n";
+        public static string welcome = "Welcome!\nWe are going to find the largest number in a collection of numbers.\nYou can define the collection yourself, or have it generated randomly.";
+        
+    }
+
+    class Program
+    {
+        public static void MessageUser(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+
+        public static void Main()
+        {
+            
+            MessageUser(Message.welcome);
+        }
+    }
+}
