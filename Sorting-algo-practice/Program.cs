15cefd845e4f130dd11b369a745350052f295aa9
Alexander Wedekind <awedwork@gmail.com>
2025-04-03 11:15:18 +0100

    adds recursive merge and quick sort functions

--- Sorting-algo-practice/Program.cs
+++ Sorting-algo-practice/Program.cs
@@ -5,7 +5,7 @@ using sortAlgosFunctionality;
 class Program
 {
     static SortAlgos sortAlgos = new SortAlgos();
-    static string algoChoice ="Choose your sorting algorithm:\n\n[1] Bubble Sort\n[2] Selection Sort\n[3] Insert Sort\n\n(use number keys to make your choice, then press ENTER)\n";
+    static string algoChoice ="Choose your sorting algorithm:\n\n[1] Bubble Sort\n[2] Selection Sort\n[3] Insert Sort\n[4] Merge Sort\n[5] Quick Sort\n\n(use number keys to make your choice, then press ENTER)\n";
     static string arrChoice = $"Do you want to:\n[1] use the preset array : {{ {unsortedArray} }}?\n[2] make your own array\n(use number keys to make your choice, then press ENTER)";
     static int[] unsortedArray = {3,6,2,7,1,4,8,5};
     static int[] sortedArray;
@@ -17,7 +17,7 @@ class Program
         return userChosenArr;
     }
 
-    static string BuildStringRepresentationOfArray(int[] arr)
+    public static string BuildStringRepresentationOfArray(int[] arr)
     {
         string arrayRepresentation = "";
         foreach(int num in arr)
@@ -66,7 +66,7 @@ class Program
     public static void Main()
     {
         Console.WriteLine("we're sorting arrays today.");
-        int choice = Menu(3, algoChoice);
+        int choice = Menu(5, algoChoice);
         switch(choice)
         {
             case 1:
@@ -77,6 +77,12 @@ class Program
                 break;
             case 3:
                 sortedArray = sortAlgos.InsertSort(unsortedArray);
+                break;
+            case 4:
+                sortedArray = sortAlgos.MergeSort(unsortedArray);
+                break;
+            case 5:
+                sortedArray = sortAlgos.QuickSort(unsortedArray);
                 break;
         }
         Console.WriteLine($"The sorted array is: {BuildStringRepresentationOfArray(sortedArray)}");
--- Sorting-algo-practice/sort-algos-functionality.cs
+++ Sorting-algo-practice/sort-algos-functionality.cs
@@ -1,7 +1,11 @@
 namespace sortAlgosFunctionality;
 
+using sortAlgoPractice;
+
 class SortAlgos
 {
+    delegate string ShowArray(int[] arr);
+    ShowArray showArray = sortAlgoPractice.Program.BuildStringRepresentationOfArray;
     public int[] BubbleSort(int[] arr)
     {
         int[] unsorted = arr;
@@ -78,5 +82,141 @@ class SortAlgos
             }
         }
         return unsorted;
+    }
+
+    public int[] MergeSort(int[] arr)
+    {
+        //Console.WriteLine($"mergesort: {showArray(arr)}");
+        if(arr.Length < 2)
+        {
+            return arr;
+        }
+
+        List<int> finished = new List<int>();
+        int halfway;
+        List<int> left = new List<int>();
+        List<int> right = new List<int>();
+
+        //Console.WriteLine($"arr length: {arr.Length}");
+        if(arr.Length % 2 == 0)
+        {
+            halfway = (arr.Length / 2) - 1;
+        }
+        else
+        {
+            halfway = arr.Length / 2;
+        }
+        //Console.WriteLine($"halfway: {halfway}");
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(i <= halfway)
+            {
+                left.Add(arr[i]);
+            }
+            else
+            {
+                right.Add(arr[i]);
+            }
+        }
+        //Console.WriteLine($"left: {showArray(left.ToArray())}\nright: {showArray(right.ToArray())}");
+
+        int[] leftUp = MergeSort(left.ToArray());
+        int[] rightUp = MergeSort(right.ToArray());
+
+        int lindex = 0;
+        int rindex = 0;
+
+        for(int i = 0; i < leftUp.Length + rightUp.Length; i++)
+        {
+            if(lindex < leftUp.Length && rindex < rightUp.Length)
+            {
+                if(leftUp[lindex] < rightUp[rindex])
+                {
+                    finished.Add(leftUp[lindex]);
+                    lindex++;
+                }
+                else if(leftUp[lindex] > rightUp[rindex])
+                {
+                    finished.Add(rightUp[rindex]);
+                    rindex++;
+                }
+                else if(leftUp[lindex] == rightUp[rindex])
+                {
+                    finished.Add(leftUp[lindex]);
+                    lindex++;
+                    finished.Add(rightUp[rindex]);
+                    rindex++;
+                }
+            }
+            else if(lindex >= leftUp.Length)
+            {
+                while(rindex < rightUp.Length)
+                {
+                    finished.Add(rightUp[rindex]);
+                    rindex++;
+                }
+            }
+            else if(rindex >= rightUp.Length)
+            {
+                while(lindex < leftUp.Length)
+                {
+                    finished.Add(leftUp[lindex]);
+                    lindex++;
+                }
+            }
+        }
+
+        return finished.ToArray();
+    }
+
+    public int[] QuickSort(int[] arr)
+    {
+            List<int> leftOntheWayDown = new List<int>();
+            int[] leftOnTheWayUp;
+            int pivot;
+            List<int> rightOnTheWayDown = new List<int>();
+            int[] rightOnTheWayUp;
+            List<int> finished = new List<int>();
+            
+            if(arr.Length < 2)
+            {
+                return arr;
+            }
+
+            pivot = arr[0];
+
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if(arr[i] <= pivot)
+                {
+                    leftOntheWayDown.Add(arr[i]);
+                }
+                else
+                {
+                    rightOnTheWayDown.Add(arr[i]);
+                }
+            }
+
+            if(leftOntheWayDown.Count > 0)
+            {
+                leftOnTheWayUp = QuickSort(leftOntheWayDown.ToArray());
+                foreach(int num in leftOnTheWayUp)
+                {
+                    finished.Add(num);
+                }
+            }
+            
+            finished.Add(pivot);
+
+            if(rightOnTheWayDown.Count > 0)
+            {
+                rightOnTheWayUp = QuickSort(rightOnTheWayDown.ToArray());
+                foreach(int num in rightOnTheWayUp)
+                {
+                    finished.Add(num);
+                }
+            }
+
+            return finished.ToArray();
     }
 }
